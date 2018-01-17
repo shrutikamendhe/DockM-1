4c1325f2cb2e87cc2ab688f2cfd6919e37b8c126
Shrutika Mendhe <shrutika.mendhe@click2cloud.net>
2018-01-17 17:02:47 +0530

    read cluster dynamically

--- script/init-ca.sh
+++ script/init-ca.sh
@@ -3,27 +3,15 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 DOCKM_HOME=$(dirname "${SCRIPTPATH}")
 
-pushd $DOCKM_HOME
-wget -O /opt/dockm/script/common.sh https://raw.githubusercontent.com/shrutikamendhe/DockM-QingCloud/dev/script/common.sh
-wget -O /opt/dockm/script/create-ca-server.sh https://raw.githubusercontent.com/shrutikamendhe/DockM-QingCloud/dev/script/create-ca-server.sh;
-chmod +x /opt/dockm/script/*
-popd
-
 source "${DOCKM_HOME}/script/common.sh"
 source "${DOCKM_HOME}/script/create-ca-server.sh"
 
 # Create a Certificate Authority (CA) server
 create_ca_server
 
-# Create a certificate for Swarm Manager
-create_node_certificate "swarm"
+cluster_conf_file="${DOCKM_HOME}/cluster-info"
 
-# Create a certificate for Node1 
-create_node_certificate "node1"
-
-# Create a certificate for Node2 
-create_node_certificate "node2"
-
-# Create a certificate for Client Manager
-create_node_certificate "client"
-
+while IFS=: read -r f1 f2
+do
+	create_node_certificate $f1
+done < "$cluster_conf_file"
